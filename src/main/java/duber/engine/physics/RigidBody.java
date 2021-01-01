97deb7bb0ba4468e505a9155ab7f48ef6ff2086e
darren <darren.lo.0530@gmail.com>
2021-01-01 03:15:05 -0500

    Added angular velocity to rigid body

--- src/main/java/duber/engine/physics/RigidBody.java
+++ src/main/java/duber/engine/physics/RigidBody.java
@@ -7,26 +7,31 @@ import duber.engine.Transform;
 public class RigidBody {
     private final Transform transform;
     private final Vector3f velocity;
+    private final Vector3f angularVelocity;
 
     public RigidBody(Transform transform) {
         this.transform = transform;
         velocity = new Vector3f();
-    }
-
-    public Vector3f getVelocity() {
-        return velocity;
+        angularVelocity = new Vector3f();
     }
     
     public Transform getTransform() {
         return transform;
     }
 
-    public void addVelocity(float x, float y, float z) {
-        velocity.add(x, y, z);
+    public Vector3f getVelocity() {
+        return velocity;
+    }
+
+    public Vector3f getAngularVelocity() {
+        return angularVelocity;
     }
 
     public void update() {
         transform.movePosition(velocity.x(), velocity.y(), velocity.z());
+        transform.rotateDegrees(angularVelocity.x(), angularVelocity.y(), angularVelocity.z());
+        
+        angularVelocity.set(0, 0, 0);
         velocity.set(0, 0, 0);
     }
 }
