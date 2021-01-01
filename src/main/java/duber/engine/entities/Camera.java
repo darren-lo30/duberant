d19db7a7a871d8cb05b11b45eeddb4dad1dbeba4
darren <darren.lo.0530@gmail.com>
2021-01-01 03:10:24 -0500

    Updated entities with entity changes

--- src/main/java/duber/engine/entities/Camera.java
+++ src/main/java/duber/engine/entities/Camera.java
@@ -1,9 +1,11 @@
 package duber.engine.entities;
 
 import org.joml.Matrix4f;
+import org.joml.Vector3f;
 
 import duber.engine.Transform;
 import duber.engine.Transformation;
+import duber.engine.Face;
 
 public class Camera extends Entity {
     private final Matrix4f viewMatrix;
@@ -15,10 +17,20 @@ public class Camera extends Entity {
 
     public void updateViewMatrix() {
         Transform cameraTransform = getTransform();
-        Transformation.updateGeneralViewMatrix(cameraTransform.getPosition(), cameraTransform.getRotationDegrees(), viewMatrix);
+        Transformation.updateGeneralViewMatrix(cameraTransform.getPosition(), cameraTransform.getRotation(), viewMatrix);
     }
 
     public Matrix4f getViewMatrix() {
         return viewMatrix;
+    }
+
+    @Override
+    public Vector3f[] getVertices() {
+        return new Vector3f[0];
+    }
+
+    @Override
+    public Face[] getFaces() {
+        return new Face[0];
     }
 }
--- src/main/java/duber/engine/entities/SkyBox.java
+++ src/main/java/duber/engine/entities/SkyBox.java
@@ -3,7 +3,7 @@ package duber.engine.entities;
 import duber.engine.exceptions.LWJGLException;
 import duber.engine.graphics.Mesh;
 
-public class SkyBox extends ConcreteEntity {
+public class SkyBox extends RenderableEntity {
     public SkyBox(Mesh skyBoxMesh) throws LWJGLException {
         super(skyBoxMesh);        
     }    
