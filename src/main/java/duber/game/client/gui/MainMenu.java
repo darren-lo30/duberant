5fe8506e664a67ac63134cd8b5eb80b032f6fb3d
goodyhi <76619937+goodyhi@users.noreply.github.com>
2021-01-21 13:20:26 -0500

    Update Main Menu

--- src/main/java/duber/game/client/gui/MainMenu.java
+++ src/main/java/duber/game/client/gui/MainMenu.java
@@ -3,19 +3,50 @@ package duber.game.client.gui;
 import java.io.IOException;
 import java.util.concurrent.BlockingQueue;
 
+import duber.engine.Window;
 import duber.engine.exceptions.LWJGLException;
 import duber.game.User;
 import duber.game.client.Duberant;
 import duber.game.client.GameStateManager.GameStateOption;
 import duber.game.networking.LoginPacket;
 import duber.game.networking.LoginConfirmationPacket;
+import org.joml.Vector2i;
+import org.liquidengine.legui.animation.AnimatorProvider;
+import org.liquidengine.legui.component.*;
+import org.liquidengine.legui.event.CursorEnterEvent;
+import org.liquidengine.legui.event.MouseClickEvent;
+import org.liquidengine.legui.listener.CursorEnterEventListener;
+import org.liquidengine.legui.listener.MouseClickEventListener;
+import org.liquidengine.legui.listener.processor.EventProcessorProvider;
+import org.liquidengine.legui.style.border.SimpleLineBorder;
+import org.liquidengine.legui.style.color.ColorConstants;
+import org.liquidengine.legui.system.context.CallbackKeeper;
+import org.liquidengine.legui.system.context.Context;
+import org.liquidengine.legui.system.context.DefaultCallbackKeeper;
+import org.liquidengine.legui.system.handler.processor.SystemEventProcessor;
+import org.liquidengine.legui.system.handler.processor.SystemEventProcessorImpl;
+import org.liquidengine.legui.system.layout.LayoutManager;
+import org.liquidengine.legui.system.renderer.Renderer;
+import org.liquidengine.legui.system.renderer.nvg.NvgRenderer;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWKeyCallbackI;
+import org.lwjgl.glfw.GLFWWindowCloseCallbackI;
+import org.lwjgl.opengl.GL;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class MainMenu extends GUI {
     private volatile boolean loggingIn = false;
-
     @Override
     public void init() throws LWJGLException {
-        // Nothing to init
+        createGui(getWindow().getDefaultInitializer().getFrame());
+        getWindow().getDefaultInitializer().getRenderer().initialize();
+       
     }
 
     @Override
@@ -33,9 +64,34 @@ public class MainMenu extends GUI {
         }
     }
 
+    public void createGui(Frame frame){
+        frame.getContainer().getStyle().getBackground().setColor(ColorConstants.lightBlue());
+        frame.getContainer().setFocusable(false);
+        Button button = new Button("Add components", 20, 20, 160, 30);
+        SimpleLineBorder border = new SimpleLineBorder(ColorConstants.black(), 1);
+        button.getStyle().setBorder(border);
+        button.getListenerMap().addListener(MouseClickEvent.class, (MouseClickEventListener) event -> {
+            
+        });
+        button.getListenerMap().addListener(CursorEnterEvent.class, (CursorEnterEventListener) System.out::println);
+
+        frame.getContainer().add(button);
+    }
     @Override
     public void render() {
-        // Nothing to render currently
+        getWindow().getDefaultInitializer().getContext().updateGlfwWindow();
+        Vector2i windowSize = getWindow().getDefaultInitializer().getContext().getFramebufferSize();
+        glClearColor(1, 1, 1, 1);
+        glViewport(0, 0, windowSize.x, windowSize.y);
+        glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+        getWindow().getDefaultInitializer().getRenderer().render(getWindow().getDefaultInitializer().getFrame(),getWindow().getDefaultInitializer().getContext());
+        glfwPollEvents();
+        glfwSwapBuffers(getWindow().getDefaultInitializer().getWindow());
+
+        getWindow().getDefaultInitializer().getSystemEventProcessor().processEvents(getWindow().getDefaultInitializer().getFrame(),getWindow().getDefaultInitializer().getContext());
+        EventProcessorProvider.getInstance().processEvents();
+        LayoutManager.getInstance().layout(getWindow().getDefaultInitializer().getFrame());
+        AnimatorProvider.getAnimator().runAnimations();
     }
 
     private class LoginRequest implements Runnable {
@@ -52,7 +108,7 @@ public class MainMenu extends GUI {
             // Send in login request to server
             try {
                 if (!getGame().isConnected()) {
-                    getGame().getClientNetwork().connect(1000);
+                    getGame().getClientNetwork().connect(100);
                 }
 
                 //Send user login packet with username
@@ -68,10 +124,13 @@ public class MainMenu extends GUI {
                 Thread.currentThread().interrupt();
             } finally {
                 loggingIn = false;
-            }            
+            }
+
+            
         }
 
         private void waitForLoginResponse() throws InterruptedException {
+            
             BlockingQueue<Object> receivedPackets = getGame().getClientNetwork().getPackets();
             Object packet = receivedPackets.take();
             if(packet instanceof LoginConfirmationPacket) {
@@ -87,6 +146,4 @@ public class MainMenu extends GUI {
             }            
         }
     }
-
-
 }
