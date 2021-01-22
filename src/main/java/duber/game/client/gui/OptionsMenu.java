381ac91177b7b7681643135a2ba393f3e5252e6e
goodyhi <76619937+goodyhi@users.noreply.github.com>
2021-01-22 03:17:02 -0500

    Update OptionsMenu.java

--- src/main/java/duber/game/client/gui/OptionsMenu.java
+++ src/main/java/duber/game/client/gui/OptionsMenu.java
@@ -1,5 +1,18 @@
 package duber.game.client.gui;
+import java.io.IOException;
+import java.util.Queue;
 
+import duber.game.User;
+import duber.game.client.GameStateManager.GameStateOption;
+import duber.game.networking.LoginPacket;
+import duber.game.networking.MatchFoundPacket;
+import duber.game.networking.MatchQueuePacket;
+import duber.game.networking.LoginConfirmationPacket;
+import org.liquidengine.legui.event.MouseClickEvent;
+import org.liquidengine.legui.event.MouseClickEvent.MouseClickAction;
+import org.liquidengine.legui.component.Button;
+import org.liquidengine.legui.style.border.SimpleLineBorder;
+import org.liquidengine.legui.style.color.ColorConstants;
 
 public class OptionsMenu extends GUI {
 
@@ -11,7 +24,20 @@ public class OptionsMenu extends GUI {
 
 	@Override
 	public void createGuiElements() {
-		// TODO Auto-generated method stub
+		getFrame().getContainer().getStyle().getBackground().setColor(ColorConstants.gray());
+        getFrame().getContainer().setFocusable(false);
+
+        Button quitButton = new Button("Quit Game", 500, 350, 480, 90);
+        SimpleLineBorder border = new SimpleLineBorder(ColorConstants.black(), 1);
+        quitButton.getStyle().setBorder(border);
+
+        quitButton.getListenerMap().addListener(MouseClickEvent.class, event -> {
+            if(event.getAction() == MouseClickAction.RELEASE ) {
+                
+            }
+        });
+
+        getFrame().getContainer().add(quitButton);
 		
 	}
 
