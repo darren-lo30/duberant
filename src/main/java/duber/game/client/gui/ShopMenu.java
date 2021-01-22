7363394f6b1435646e2aa2e00d795fcbdc807e36
goodyhi <76619937+goodyhi@users.noreply.github.com>
2021-01-22 03:17:36 -0500

    Fixed shopMenu, need to add funcs for each buy

--- src/main/java/duber/game/client/gui/ShopMenu.java
+++ src/main/java/duber/game/client/gui/ShopMenu.java
@@ -2,7 +2,20 @@ package duber.game.client.gui;
 
 import duber.game.client.GameStateManager.GameStateOption;
 import duber.game.client.match.Match;
-
+import duber.game.User;
+import duber.game.client.GameStateManager.GameStateOption;
+import duber.game.networking.LoginPacket;
+import duber.game.networking.MatchFoundPacket;
+import duber.game.networking.MatchQueuePacket;
+import duber.game.networking.LoginConfirmationPacket;
+import org.liquidengine.legui.event.MouseClickEvent;
+import org.liquidengine.legui.event.MouseClickEvent.MouseClickAction;
+import org.liquidengine.legui.component.Button;
+import org.liquidengine.legui.component.TextArea;
+import org.liquidengine.legui.style.border.SimpleLineBorder;
+import org.liquidengine.legui.style.color.ColorConstants;
+import static org.liquidengine.legui.component.optional.align.HorizontalAlign.CENTER;
+import static org.liquidengine.legui.component.optional.align.VerticalAlign.BOTTOM;
 public class ShopMenu extends GUI {
     private Match match;
 
@@ -27,7 +40,42 @@ public class ShopMenu extends GUI {
 
     @Override
     public void createGuiElements() {
-        // TODO Auto-generated method stub
+        getFrame().getContainer().getStyle().getBackground().setColor(ColorConstants.gray());
+        getFrame().getContainer().setFocusable(false);
+
+        
+
+        Button smgButton = new Button("SMG", 20, 20, 160, 30);
+        SimpleLineBorder border = new SimpleLineBorder(ColorConstants.black(), 1);
+        smgButton.getStyle().setBorder(border);
+
+        smgButton.getListenerMap().addListener(MouseClickEvent.class, event -> {
+            if(event.getAction() == MouseClickAction.RELEASE ) {
+                
+            }
+        });
+        Button lmgButton = new Button("LMG", 100, 20, 160, 30);
+        smgButton.getStyle().setBorder(border);
+
+        smgButton.getListenerMap().addListener(MouseClickEvent.class, event -> {
+            if(event.getAction() == MouseClickAction.RELEASE ) {
+                
+            }
+        });
+        Button arButton = new Button("AR", 180, 20, 160, 30);
+        smgButton.getStyle().setBorder(border);
+
+        smgButton.getListenerMap().addListener(MouseClickEvent.class, event -> {
+            if(event.getAction() == MouseClickAction.RELEASE ) {
+                
+            }
+        });
+
+        
+        getFrame().getContainer().add(smgButton);
+        getFrame().getContainer().add(arButton);
+        getFrame().getContainer().add(lmgButton);
+        
 
     }
     
