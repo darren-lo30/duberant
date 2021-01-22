9c33042994483806befaf046c433ba0b7948a8ae
goodyhi <76619937+goodyhi@users.noreply.github.com>
2021-01-22 03:17:21 -0500

    Made ScoreboardDisplay (Need to make look better)

--- src/main/java/duber/game/client/gui/ScoreboardDisplay.java
+++ src/main/java/duber/game/client/gui/ScoreboardDisplay.java
@@ -3,7 +3,12 @@ package duber.game.client.gui;
 import duber.game.client.GameStateManager.GameStateOption;
 import duber.game.client.match.Match;
 import duber.game.gameobjects.Scoreboard;
+import duber.engine.entities.components.Named;
 
+import org.liquidengine.legui.component.TextArea;
+
+import static org.liquidengine.legui.component.optional.align.HorizontalAlign.CENTER;
+import static org.liquidengine.legui.component.optional.align.VerticalAlign.BOTTOM;
 public class ScoreboardDisplay extends GUI {
     @Override
     public void enter() {
@@ -32,7 +37,58 @@ public class ScoreboardDisplay extends GUI {
 
     @Override
     public void createGuiElements() {
-        // TODO Auto-generated method stub
+        int team=0;
+        TextArea namesText= new TextArea(420,280,100,250);
+        TextArea killsText= new TextArea(520,280,100,250);
+        TextArea deathsText=new TextArea(620,280,100,250);
+        TextArea scoreboardText=new TextArea(420,230,300,50);
+        scoreboardText.getTextAreaField().getStyle().setHorizontalAlign(CENTER);
+        namesText.setVerticalScrollBarVisible(false);
+        namesText.setPressed(false);
+        namesText.setHorizontalScrollBarVisible(false);
+        killsText.setVerticalScrollBarVisible(false);
+        killsText.setHorizontalScrollBarVisible(false);
+        deathsText.setVerticalScrollBarVisible(false);
+        deathsText.setHorizontalScrollBarVisible(false);
+        scoreboardText.setVerticalScrollBarVisible(false);
+        scoreboardText.setHorizontalScrollBarVisible(false);
+        scoreboardText.getTextState().setText("SCORES");
+        namesText.setEditable(false);
+        killsText.setEditable(false);
+        deathsText.setEditable(false);
+        String currName="Name\n";
+        String currKills="Kills\n";
+        String currDeaths="Deaths\n";
+        for(int i=0; i<10; i++){
+            if(i==5){
+                team =1;
+                currName=currName+"\n";
+                currKills=currKills+"\n";
+                currDeaths=currDeaths+"\n";
+            }
+            int kills= getMatchScoreboard().getScores(team).get(i).getKills();
+            int deaths= getMatchScoreboard().getScores(team).get(i).getDeaths();
+            String name= getMatchScoreboard().getScores(team).get(i).getEntity().getComponent(Named.class).getName();
+            currName=currName+name+"\n";
+            currKills= currKills+Integer.toString(kills)+"\n";
+            currDeaths= currDeaths+Integer.toString(deaths)+"\n";
+        }
+        namesText.getTextState().setText(currName);
+        killsText.getTextState().setText(currKills);
+        deathsText.getTextState().setText(currDeaths);
+        namesText.setCaretPosition(12);
+        namesText.getTextAreaField().getStyle().setHorizontalAlign(CENTER);
+        namesText.getTextAreaField().getStyle().setVerticalAlign(BOTTOM);
+        killsText.setCaretPosition(12);
+        killsText.getTextAreaField().getStyle().setHorizontalAlign(CENTER);
+        killsText.getTextAreaField().getStyle().setVerticalAlign(BOTTOM);
+        deathsText.setCaretPosition(12);
+        deathsText.getTextAreaField().getStyle().setHorizontalAlign(CENTER);
+        deathsText.getTextAreaField().getStyle().setVerticalAlign(BOTTOM);
+        getFrame().getContainer().add(namesText);
+        getFrame().getContainer().add(killsText);
+        getFrame().getContainer().add(deathsText);
+        getFrame().getContainer().add(scoreboardText);
 
     }
     
